6245e168634dbed91b97a8d57ae9f8a2adafd1e0
Alexandre Gueleres <ale.gueleres@gmail.com>
2018-01-19 10:23:14 -0200

    Migrate to github from collab

---
+++ Assets/GameLevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLevelManager : MonoBehaviour {
+
+    private static int score;
+    public Text text;
+    public bool isGameOver = false;
+    private bool scanPath = true;
+
+    void Start()
+    {
+        StartCoroutine(updateScoreManager(1f, 1));
+    }
+
+    void Update()
+    {
+        if (scanPath)
+        {
+            AstarPath.active.Scan();
+            scanPath = false;
+        }
+        text.text = "Pontuação: " + score.ToString();
+    }
+
+    IEnumerator updateScoreManager(float totalTime, int amount)
+    {
+        while (!isGameOver)
+        {
+            addScore(amount);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public static void addScore(int amount)
+    {
+        score += amount;
+    }
+
+}
---
+++ Assets/MazeGenerator/Scripts/MazeScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeScript : MonoBehaviour {
+
+	public AudioClip JumpSound = null;
+	public AudioClip CoinSound = null;
+
+	private Rigidbody mRigidBody = null;
+	private AudioSource mAudioSource = null;
+	private bool mFloorTouched = false;
+
+    void Start () {
+		mRigidBody = GetComponent<Rigidbody> ();
+		mAudioSource = GetComponent<AudioSource> ();
+    }
+
+	void OnCollisionExit(Collision coll){
+		if (coll.gameObject.tag.Equals ("Floor")) {
+			mFloorTouched = false;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Coin")) {
+			if(mAudioSource != null && CoinSound != null){
+				mAudioSource.PlayOneShot(CoinSound);
+			}
+            GameLevelManager.addScore(10);
+			Destroy(other.gameObject);
+		}
+	}
+}
