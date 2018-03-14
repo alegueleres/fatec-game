dc436cc8cc6c2544480e55e87862212150762d63
Alexandre Gueleres <ale.gueleres@gmail.com>
2018-03-14 00:33:20 -0300

    Add enemy and butterfly that helps to find the exit

---
+++ Assets/Enemy/EnemyScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    private Animator animator;
+
+    public GameObject enemy;
+
+    void Start()
+    {
+        animator = enemy.GetComponent<Animator>();
+    }
+
+    void Update () {
+        if (animator.GetBool("enemyDied"))
+        {
+            AIPath aipath = enemy.GetComponent<AIPath>();
+            aipath.canMove = false;
+            aipath.canSearch = false;
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        Debug.Log(collider.tag == "Hero");
+        if (collider.tag == "Hero")
+        {
+            animator.SetBool("enemyAttack", true);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        animator.SetBool("enemyAttack", false);
+    }
+}
